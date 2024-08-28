6ea546a42e54e1b0086f335708f92c291e49ba93
Haruna2004 <harunafaruk2004@gmail.com>
2024-08-28 13:32:43 +0100

    initial commit

---
+++ public/assets/index.ts
@@ -0,0 +1,27 @@
+import next_logo from "../next.svg";
+import vercel_logo from "../vercel.svg";
+import app_logo from "./app_logo.svg";
+import bg_image from "./bg-image.jpg";
+
+import hospital_1 from "./home/hospital-1.jpg";
+import hospital_2 from "./home/hospital-2.jpg";
+import hospital_3 from "./home/hospital-3.jpg";
+import driver_image from "./home/driver-image.jpeg";
+
+//
+
+import phone_icon from "./home/phone_icon.svg";
+import google_map_icon from "./home/google_map_icon.svg";
+
+export {
+  next_logo,
+  vercel_logo,
+  app_logo,
+  hospital_1,
+  hospital_2,
+  hospital_3,
+  phone_icon,
+  google_map_icon,
+  driver_image,
+  bg_image,
+};
